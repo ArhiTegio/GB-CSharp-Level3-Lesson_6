27e210235e0ee87687d63030d114230e5be8c741
Arhio <Arhio@yandex.ru>
2020-01-13 23:37:39 +0300

    C# Уровень 3. Урок 6. Задание 2 выполнено.

---
+++ FunctionalFiles/FunctionalFiles/Model/Model.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalFiles.Model
+{
+    public class ModelFuncFiles
+    {
+        private string path = @"C:\";
+        Random random = new Random();
+
+        Queue<string> texts = new Queue<string>();
+
+        public string Path { get => path; set => path = value; }
+
+        public void ClearPath()
+        {
+            Parallel.ForEach(Directory.EnumerateDirectories(Path),  x => Directory.Delete(x));
+            Parallel.ForEach(Directory.EnumerateFiles(Path), x => File.Delete(x));
+        }
+
+        public void FillPath()
+        {
+            Parallel.For(0, 1000, i => { try{ File.WriteAllText($@"{Path}\{i}.txt", $"{random.Next(1, 3)} {random.NextDouble() * random.Next(1, 10)} {random.NextDouble() * random.Next(1, 10)}"); } catch { } });            
+        }
+         
+        public async void CalcPath()
+        {
+            var a = Directory.EnumerateFiles(Path);
+            File.Create($@"{Path}\result.dat");
+            await Task.Run(() =>
+            {
+                Parallel.ForEach(a, x =>
+                {
+
+                    var elements = new double[0];
+                    try
+                    {
+                        elements = File.ReadAllText(x).Split(' ').Select(y => double.Parse(y)).ToArray();
+                    }
+                    catch { }
+                    if (elements != null && elements.Length >= 3)
+                        if (elements[0] < 1.5) texts.Enqueue($"{x} {(elements[1] * elements[2]).ToString()} {Environment.NewLine}");
+                        else texts.Enqueue($"{x} {(elements[1] / elements[2]).ToString()} {Environment.NewLine}");
+                });
+            });
+
+            await Task.Run(() => 
+            {
+                while(texts.Count != 0)
+                {
+                    File.AppendAllText($@"{Path}\result.dat", texts.Dequeue());
+                }
+            });
+        }
+    }
+}
---
+++ FunctionalFiles/FunctionalFiles/ViewModel/Functional.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FunctionalFiles.Model;
+using WinForms = System.Windows.Forms;
+
+namespace FunctionalFiles.ViewModel
+{
+    public class Functional : ViewModelBase
+    {
+        private ModelFuncFiles model;
+
+        public string Dir
+        {
+            get => model.Path;
+            set
+            {
+                model.Path = value; OnPropertyChanged(nameof(Dir));
+            }
+        }
+
+        public Functional()
+        {
+            model = new ModelFuncFiles();
+        }
+
+        private RelayCommand _calcCommand;
+        public RelayCommand CalcCommand => _calcCommand ?? (_calcCommand =
+                                    new RelayCommand(x => model.CalcPath()));
+
+        private RelayCommand _newFilesCommand;
+        
+        public RelayCommand NewFilesCommand => _newFilesCommand ?? (_newFilesCommand =
+                                    new RelayCommand(x => { model.ClearPath();  model.FillPath(); }));
+        
+        private RelayCommand _dirCommand;
+
+        public RelayCommand DirCommand => _dirCommand ?? (_dirCommand =
+                                    new RelayCommand(x => 
+                                    { 
+                                        var fbd = new WinForms.FolderBrowserDialog();
+                                        if (fbd.ShowDialog() == DialogResult.OK) Dir = fbd.SelectedPath;                                        
+                                    }));
+    }
+}
